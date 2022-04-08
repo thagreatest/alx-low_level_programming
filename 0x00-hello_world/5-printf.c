bf55f8b447437a57e28b4928c6bf057b027a42df
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-08 01:56:43 +0100

    Printing the proper grammer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+* main - Function begins
+*
+* Return: Function ends
+*/
+int main(void)
+{
+printf("with proper grammer,but the outcome is a piece of art,\n");
+return(0)
+}
