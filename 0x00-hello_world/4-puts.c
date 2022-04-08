98b72b9ff59d777a1ebc99029df52b83981c4c4c
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-08 01:36:06 +0100

    putting an hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - entry point
-*
-*Return: Always 0 (Success)
-*/
-
+ *main - Function begins
+ *
+ *Return: Function ends (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building multilingual puzzle");
-return (0);
+    puts("\"Programming is like building multilingual puzzle");
+    return (0);
 }
