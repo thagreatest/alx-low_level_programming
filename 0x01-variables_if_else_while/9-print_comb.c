5f441843a3f5a07c7efbb3770c8885f39e86e8a9
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-09 02:03:07 +0100

    All single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all possible combination of single digit numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c = '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
