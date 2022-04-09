abd81f1e1bbbe676df34589679495dbb895b5eac
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-09 11:46:01 +0100

    Printing three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-	int a, b, c;
+int a, b, c;
 
-	for (a = 0; a < 8; a++)
-	{
-		for (b = a + 1; b < 9; b++)
-		{
-			for (c = b + 1; b < 10; c++)
-			{
-				putchar((a % 10) + '0');
-				putchar((b % 10) + '0');
-				putchar((c % 10) + '0');
+for (a = 0; a < 8; a++)
+{
+for (b = a + 1; b < 9; b++)
+{
+for (c = b + 1; b < 10; c++)
+{
+putchar((a % 10) + '0');
+putchar((b % 10) + '0');
+putchar((c % 10) + '0');
 
-				if (a == 7 && b == 8 && c == 9)
-					continue;
+if (a == 7 && b == 8 && c == 9)
+continue;
 
-					putchar(',');
-					putchar(' ');
-			}
-		}
-	}
+putchar(',');
+putchar(' ');
+}
+}
+}
 
-	putchar('\n');
+putchar('\n');
 
-	retutn (0);
+return (0);
 }
