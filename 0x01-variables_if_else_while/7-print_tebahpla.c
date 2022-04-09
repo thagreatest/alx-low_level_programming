b1bc08944cf061c8a25dd74470cb2942c7c7ea57
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-09 01:28:56 +0100

    Going Backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Function begins
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
