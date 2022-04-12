61f01f30aeb7b7dc5cdbfb017f295726e94087af
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-13 00:31:14 +0100

    making the characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,16 @@
  * Return: Nothing to see here
  */
 
-int main(void) {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+int main(void)
+{
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-    return (0);
+return (0);
 }
