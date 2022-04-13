ab307fce07e112d16284323be38361d249908c22
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-13 03:51:21 +0100

    Printing the alphabets

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign
+ * @n: number to check
+ * Return: 0 or 1
+ */
+
+int print_sign(int n)
+{
+int test;
+
+if (n > 0);
+{
+test = 1;
+_putchar('+');
+}
+else
+{
+test = -1
+_putchar('-');
+}
+
+return (test);
+}
