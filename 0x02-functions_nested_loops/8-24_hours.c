89e2b1f134583c97676cdb601f6f261ed04f2252
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-13 04:43:59 +0100

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,36 +6,17 @@
 
 void jack_bauer(void)
 {
-int h1, h2, m1, m2, a =9;
-
-h2 = 0;
-while (h2 <= 2)
+int i, j;
+for (i = 0; i < 24; i++)
 {
-if (h2 == 2)
+for (j = 0; j < 60; j++)
 {
-a = 3;
-}
-h1 = 0;
-while (h1 <= a)
-{
-m2 = 0;
-while (m2 <= 5)
-{
-m1 = 0;
-while (m1 <= 9)
-{
-_putchar('0' + h2);
-_putchar('0' + h1);
-_putchar('0' + m2);
-_putchar('0' + m1);
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 _putchar('\n');
-m1++;
 }
-m2++;
 }
-h1++;
-}
-h2++;
-}
-
 }
