952cd1237e704ac9271f745259ef74807a70948a
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-13 01:30:46 +0100

    Printing alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@ void print_alphabet_x10(void)
 char c;
 int i = 0;
 while (i < 10)
-    {
-        c  = 'a';
-        while (c <= 'z')
-        {   
-            _putchar(c);
-            c++;
-        }
-        _putchar('\n');
-        i++;
-    }
+{
+c  = 'a';
+while (c <= 'z')
+{  
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
 }
