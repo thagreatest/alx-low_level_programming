aee437c148c44a50fc8c14e2ecd1e9ffcf4ad9a0
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-13 01:13:13 +0100

    Printing the alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,13 @@
 
 void print_alphabet(void)
 {
-    char c = 'a';
+char c = 'a';
 
-    while (c <= 'z')
-    {
-        _putchar(c);
-        c++;
-    }
-    _putchar('\n');
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+
 }
