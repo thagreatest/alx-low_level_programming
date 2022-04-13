96615f08beb4ea23599170810bf17acc523d0e4e
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-13 02:32:56 +0100

    Checking if it is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,10 @@
 #include "main.h"
 /**
  *  main - Checks the code
- * @c : character to check the case
+ *@c : character to check the case
  *  Return: Always 0 or 1
  */
 
-int _islower(int m);
-{
-
-if (m >= 'a' && m <= 'z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+int _islower(int m) {
+    return (m >= 97 && m <= 122);
 }
