85c5c7f2f2a2f972a8cc6876307c43087af77021
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-21 02:29:05 +0100

    Let us know the length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  _strlen - Reeturns the length of the string
+ * *@s : input
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(*s != 0)
+	{
+		length++;
+		s++;
+	}
+
+	return (lenth);
+}
+
