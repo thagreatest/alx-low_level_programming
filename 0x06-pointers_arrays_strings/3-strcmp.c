53381857118759e8b53aa4f6fd8e34ab8dea24a6
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-04-24 20:42:52 +0100

    Comparing the strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 /**
  * _strcmp - compares two strings
@@ -7,13 +6,12 @@
  *
  * Return: 0 if equal or b if is different
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 	int b = 0;
 
-	while(s1[a] != '\0' && b == 0)
+	while (s1[a] != '\0' && b == 0)
 	{
 		b = s1[a] - s2[a];
 		a++;
