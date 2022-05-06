a6b5dbc6e603f3e53f85a3c162494627c066b407
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-05-06 01:55:22 +0100

    Creating an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(char) * size)
+	arr = malloc(sizeof(char) * size);
 
 	/* Checking to see if malloc works */
 
