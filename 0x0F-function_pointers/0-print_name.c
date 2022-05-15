d62f7e6e385f7eb54379b27d6e83ef754a67905b
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-05-15 22:17:02 +0100

    Printing a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include "function_pointers.h
+#include "function_pointers.h"
 
 /**
- * print_name - Printing a name
- * @name: name in question
- * @f: pointer
- * Return: Nothing
- **/
-
-void print_name(char *name, void (*f)(char *c))
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: is pointer
+ * Return: Nothing.
+**/
+void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 {
