a06aafde5e643077eae9f5e1a306c4bb5dcf8b46
Akinbobola Akingbeja <akingbejabobola@gmail.com>
2022-05-25 11:59:41 +0100

    Printing a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,10 +18,10 @@ size_t print_list(const list_t *m)
 			printf("[0] (nil)\n");
 
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", m->len, m->str);
 
 		nodes++;
-		h = h->next;
+		m = m->next;
 	}
 
 	return (nodes);
